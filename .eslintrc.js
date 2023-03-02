c95385c23165cc42a6a251a4f209ee4b16558ebe
VicvekSr <Olamideoso65@gmail.com>
2023-03-02 09:10:08 +0000

    add  "no-unused-vars": "off" to ESLint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
       'no-underscore-dangle': 'off',
       'no-console': 'off',
       'no-shadow': 'off',
+      "no-unused-vars": "off",
       'no-restricted-syntax': [
         'error',
         'LabeledStatement',
