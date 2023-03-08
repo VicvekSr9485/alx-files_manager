0a4c52f0d76ec8068dd28571bb71163fb7861ac8
VicvekSr <Olamideoso65@gmail.com>
2023-03-08 19:17:06 +0000

    server.js restored

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 import express from 'express';
-
-const routes = require('./routes/index');
+import router from './routes';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// adding this to decode json post bodies
+app.use(express.json());
+app.use(router);
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-// adding this to decode json post bodies
-app.use(express.json());
-
-app.use('/', routes);
-
-export default app;
